c2750823d4d95e3b75dbd9a8e6957a1156220936
adexphem <adexphem4real@gmail.com>
2018-10-02 12:38:00 +0100

    adding now serve for deployment

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import Moment from 'react-moment';
 import Spinner from '../layout/Spinner';
 
 class Lyrics extends Component {
@@ -31,10 +32,27 @@ class Lyrics extends Component {
         <React.Fragment>
           <Link to="/" className="btn btn-dark btn-sm mb-4">Back</Link>
           <div className="card">
-            <div className="card-header">
+            <h4 className="card-header">
               {track.track_name} by <span className="text-secondary">{track.artist_name}</span>
+            </h4>
+            <div className="card-body">
+              <p className="card-text">
+                {lyrics.lyrics_body}
+              </p>
             </div>
           </div>
+
+          <ul className="list-group mt-3">
+            <li className="list-group-item">
+              <strong>Album ID </strong>: {track.album_id}
+            </li>
+            <li className="list-group-item">
+              <strong>Song Genre </strong>: {track.primary_genres.music_genre_list[0].music_genre.music_genre_name}
+            </li>
+            <li className="list-group-item">
+              <strong>Release Date </strong>: <Moment format="YYYY/MM/DD">{track.first_release_date}</Moment>
+            </li>
+          </ul>
         </React.Fragment>
       )
     } else {
